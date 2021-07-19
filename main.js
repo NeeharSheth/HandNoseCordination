bdb0373ea8a9c9dbbb33aad6dd363f9d7305c004
NeeharSheth <75029741+NeeharSheth@users.noreply.github.com>
2021-07-19 12:50:45 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+console.log("ml5 version:",ml5.version);
+
 function preload(){
 
 }
@@ -5,8 +7,23 @@ function preload(){
 function setup(){
     canvas= createCanvas(500,500);
     canvas.position(700,210);
+    video= createCapture(VIDEO);
+    video.size(550,500);
+    video.position(0,210);
+    posenet= ml5.poseNet(video, modelLoaded);
+    posenet.on('pose',gotPoses)
 }
 
 function draw(){
 
+}
+
+function modelLoaded(){
+    console.log("posenet is loaded :D")
+}
+
+function gotPoses(result){
+    if(result.length>0){
+        console.log(result);
+    }
 }
