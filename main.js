44a8ea97544a48795ea43104aa324096b3430c27
NeeharSheth <75029741+NeeharSheth@users.noreply.github.com>
2021-07-20 15:01:37 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 console.log("ml5 version:",ml5.version);
 
+noseX= 0;
+noseY= 0;
+difference=0;
+leftWrist= 0;
+rightWrist=0;
+ 
 function preload(){
 
 }
@@ -11,11 +17,15 @@ function setup(){
     video.size(550,500);
     video.position(0,210);
     posenet= ml5.poseNet(video, modelLoaded);
-    posenet.on('pose',gotPoses)
+    posenet.on('pose',gotPoses);
 }
 
 function draw(){
-
+    document.getElementById("result").innerHTML= "Width and Height of the square is: "+difference+" pixels";
+    background("#969A97");
+    stroke("#B4846C");
+    fill("#B4846C");
+square(noseX,noseY,difference);
 }
 
 function modelLoaded(){
@@ -25,5 +35,10 @@ function modelLoaded(){
 function gotPoses(result){
     if(result.length>0){
         console.log(result);
+        noseX= result[0].pose.nose.x;
+        noseY= result[0].pose.nose.y;
+        leftWrist= result[0].pose.leftWrist.x;
+        rightWrist= result[0].pose.rightWrist.x;
+        difference= abs(floor(leftWrist-rightWrist));
     }
 }
